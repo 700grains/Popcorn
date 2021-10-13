2b4655911e94ce0577d900c5915358e2f4eab873
700grains <60423788+700grains@users.noreply.github.com>
2021-10-13 16:57:27 +0300

    Creating color for border pen&Brush

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -18,8 +18,8 @@ enum EBrick_Type
 };
 
 HWND Hwnd;
-HPEN Highlight_Pen, Letter_Pen, BG_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Ball_Pen;
-HBRUSH BG_Brush, Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush, Ball_Brush;
+HPEN Highlight_Pen, Letter_Pen, BG_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Ball_Pen, Border_Blue_Pen, Border_White_Pen;
+HBRUSH BG_Brush, Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush, Ball_Brush, Border_Blue_Brush, Border_White_Brush;
 
 const int Global_Scale = 3;
 const int Brick_Width = 15;
@@ -101,6 +101,9 @@ void Init_Engine(HWND hwnd)
 	Create_Pen_Brush(151, 0, 0, Platform_Circle_Pen, Platform_Circle_Brush);
 	Create_Pen_Brush(0, 128, 192, Platform_Inner_Pen, Platform_Inner_Brush);
 	Create_Pen_Brush(255, 255, 255, Ball_Pen, Ball_Brush);
+	Create_Pen_Brush(85, 255, 255, Border_Blue_Pen, Border_Blue_Brush);
+	Create_Pen_Brush(255, 255, 255, Border_White_Pen, Border_White_Brush);
+
 
 	Level_Rect.left = Level_X_Offset * Global_Scale;
 	Level_Rect.top = Level_Y_Offset * Global_Scale;
@@ -333,6 +336,9 @@ void Draw_Frame(HDC hdc, RECT &paint_area)
 
 	if (IntersectRect(&intersection_rect, &paint_area, &Ball_Rect) )
 		Draw_Ball(hdc, paint_area);
+
+	SelectObject(hdc, Border_Blue_Pen);
+	SelectObject(hdc, Border_Blue_Brush);
 }
 //------------------------------------------------------------------------------------------------------------
 int On_Key_Down(EKey_Type key_type)
