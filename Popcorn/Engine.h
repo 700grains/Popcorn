485f9c8652d6de530d045aee31c67212117cfff3
700grains <60423788+700grains@users.noreply.github.com>
2021-10-14 11:28:46 +0300

    Moved some ALevel class methods to private section

--- Popcorn/Engine.h
+++ Popcorn/Engine.h
@@ -57,16 +57,7 @@ class ALevel
 public:
 	void init();
 	void Check_Level_Brick_Hit(int& next_y_pos, double& ball_direction);
-
-	void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
-	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN& front_pen, HBRUSH& front_brush, HPEN& back_pen, HBRUSH& back_brush);
-	void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
 	void Draw_Level(HDC hdc, RECT& paint_area);
-
-	HPEN   Brick_Red_Pen, Brick_Blue_Pen, Letter_Pen;
-	HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
-	RECT Level_Rect;
-
 
 	static const int Level_Width = 12;  // Ширина уровня в ячейках
 	static const int Level_Height = 14;  // Высота уровня в ячейках
@@ -74,6 +65,17 @@ public:
 	static const int Level_Y_Offset = 6;
 	static const int Cell_Width = 16;
 	static const int Cell_Height = 8;
+
+private:
+	void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
+	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN& front_pen, HBRUSH& front_brush, HPEN& back_pen, HBRUSH& back_brush);
+	void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
+
+	HPEN   Brick_Red_Pen, Brick_Blue_Pen, Letter_Pen;
+	HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
+	RECT Level_Rect;
+
+
 	static const int Brick_Width = 15;
 	static const int Brick_Height = 7;
 };
