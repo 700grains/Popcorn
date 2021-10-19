5e816dc20474ffc4de62b3cdb9faa33fe6641b8e
700grains <60423788+700grains@users.noreply.github.com>
2021-10-19 12:08:04 +0300

    '18 finished & fading brick is done

--- Popcorn/Level.h
+++ Popcorn/Level.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "Config.h"
+#include "Active_Brick.h"
 
 //------------------------------------------------------------------------------------------------------------
 enum EBrick_Type
@@ -24,7 +25,9 @@ public:
 
 	void Init();
 	void Check_Level_Brick_Hit(int &next_y_pos, double &ball_direction);
-	void Draw(HDC hdc, RECT &paint_area);
+	void Draw(HWND hwnd, HDC hdc, RECT &paint_area);
+
+	AActive_Brick Active_Brick;
 
 private:
 	void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
@@ -34,9 +37,9 @@ private:
 	HPEN Brick_Red_Pen, Brick_Blue_Pen, Letter_Pen;
 	HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
 	RECT Level_Rect;
+
 	static char Level_01[AsConfig::Level_Height][AsConfig::Level_Width];
 
-	static const int Brick_Width = 15;
-	static const int Brick_Height = 7;
+
 };
 //------------------------------------------------------------------------------------------------------------
