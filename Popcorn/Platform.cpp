c937a3fce36a0471f7481fa6e437e6aeeb2e6be1
700grains <60423788+700grains@users.noreply.github.com>
2023-01-22 22:18:38 +0300

    draw a spot of glue

--- Popcorn/Platform.cpp
+++ Popcorn/Platform.cpp
@@ -484,6 +484,20 @@ void AsPlatform::Draw_Expanding_Roll_In_State(HDC hdc, RECT& paint_area)
 void AsPlatform::Draw_Glue_State(HDC hdc, RECT& paint_area)
 {// draw a platform with spreading glue
 
+	RECT spot_rect;
+	int platform_top = (AsConfig::Platform_Y_Pos + 1) * AsConfig::Global_Scale;
+	int spot_height = 5 * AsConfig::Global_Scale;
+
+	Draw_Normal_State(hdc, paint_area);
+
+	// draw a spot of glue
+	AsConfig::White_Color.Select(hdc);
+	spot_rect.left = (int) ((X_Pos + 5.0) * AsConfig::D_Global_Scale);
+	spot_rect.top = platform_top - spot_height;
+	spot_rect.right = spot_rect.left + 11 * AsConfig::Global_Scale;
+	spot_rect.bottom = platform_top + spot_height;
+
+	Chord(hdc, spot_rect.left, spot_rect.top, spot_rect.right - 1, spot_rect.bottom - 1, spot_rect.left, platform_top, spot_rect.right - 1, platform_top);
 }
 //------------------------------------------------------------------------------------------------------------
 bool AsPlatform::Reflect_On_Circle(double next_x_pos, double next_y_pos, double platform_ball_x_offset, ABall* ball)
