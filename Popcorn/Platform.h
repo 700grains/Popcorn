4442e54bfccbcff42eb00ee3c6eade423010b0d6
700grains <60423788+700grains@users.noreply.github.com>
2023-01-24 00:56:28 +0300

    Move the platform glue states to a separate enumeration

--- Popcorn/Platform.h
+++ Popcorn/Platform.h
@@ -13,9 +13,16 @@ enum EPlatform_State
 	EPS_Meltdown,
 	EPS_Roll_In,
 	EPS_Expand_Roll_In,
-	EPS_Glue_Init,
 	EPS_Glue,
-	EPS_Glue_Finalize,
+};
+//------------------------------------------------------------------------------------------------------------
+enum EPlatform_Substate_Glue
+{
+	EPSG_Unknown,
+
+	EPSG_Init,
+	EPSG_Active,
+	EPSG_Finalize,
 };
 //------------------------------------------------------------------------------------------------------------
 enum EPlatform_Moving_State
@@ -69,6 +76,7 @@ private:
 	void Get_Normal_Platform_Image(HDC hdc);
 
 	EPlatform_State Platform_State;
+	EPlatform_Substate_Glue Platform_Substate_Glue;
 	EPlatform_Moving_State Platform_Moving_State;
 	bool Right_Key_Down, Left_Key_Down;
 	int Inner_Width;
