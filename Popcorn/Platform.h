5ad77bca8d26f94960e5a3f381a303319c792b97
700grains <60423788+700grains@users.noreply.github.com>
2023-01-28 19:20:32 +0300

    Create classes ALaser_Beam and AsLaser_Beam_Set

--- Popcorn/Platform.h
+++ Popcorn/Platform.h
@@ -137,6 +137,41 @@ private:
 	static const double Min_Expanding_Platform_Width, Max_Expanding_Platform_Width, Expanding_Platform_Width_Step;
 };
 //------------------------------------------------------------------------------------------------------------
+class ALaser_Beam : public AMover, public AGraphics_Object
+{
+public:
+	ALaser_Beam();
+
+	virtual void Begin_Movement();
+	virtual void Finish_Movement();
+	virtual void Advance(double max_speed);
+	virtual double Get_Speed();
+
+	virtual void Act();
+	virtual void Clear(HDC hdc, RECT& paint_area);
+	virtual void Draw(HDC hdc, RECT& paint_area);
+	virtual bool Is_Finished();
+};
+//------------------------------------------------------------------------------------------------------------
+class AsLaser_Beam_Set : public AMover, public AGraphics_Object
+{
+public:
+	virtual void Begin_Movement();
+	virtual void Finish_Movement();
+	virtual void Advance(double max_speed);
+	virtual double Get_Speed();
+
+	virtual void Act();
+	virtual void Clear(HDC hdc, RECT& paint_area);
+	virtual void Draw(HDC hdc, RECT& paint_area);
+	virtual bool Is_Finished();
+
+private:
+	static const int Max_Laser_Beam_Count = 10;
+
+	ALaser_Beam Laser_Beams[Max_Laser_Beam_Count];
+};
+//------------------------------------------------------------------------------------------------------------
 class AsPlatform_Laser
 {
 public:
@@ -162,10 +197,7 @@ private:
 	AsPlatform_State* Platform_State;
 	AColor* Inner_Color, * Circle_Color; // UNO, Use, Not Own!
 	AColor* Gun_Color;
-
-	static const int Max_Laser_Beam_Count = 10;
-
-	ALaser_Beam Laser_Beams[Max_Laser_Beam_Count];
+	AsLaser_Beam_Set Laser_Beam_Set;
 
 	int Laser_Transformation_Step;
 
