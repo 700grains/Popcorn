0c83b771e37526b3b6d4cb82dce71a5c85a4bb71
700grains <60423788+700grains@users.noreply.github.com>
2023-01-30 00:50:06 +0300

    Moving the AsPlatform_Laser class to a separate pair of files

--- Popcorn/Platform.h
+++ Popcorn/Platform.h
@@ -5,43 +5,9 @@
 #include "Platform_State.h"
 #include "Platform_Glue.h"
 #include "Platform_Expanding.h"
+#include "Platform_Laser.h"
 
 
-//------------------------------------------------------------------------------------------------------------
-class AsPlatform_Laser
-{
-public:
-	~AsPlatform_Laser();
-	AsPlatform_Laser(AsPlatform_State &platform_state);
-
-	void Init(AsLaser_Beam_Set *laser_beam_set, AColor &highlight_color, AColor& circle_color, AColor& inner_color);
-	bool Act(EPlatform_State& next_state, double x_pos);
-	void Draw_State(HDC hdc, double x_pos, RECT& platform_rect);
-	void Reset();
-	void Fire(bool fire_on);
-
-private:
-	void Draw_Laser_Wing(HDC hdc, double x_pos, bool is_left);
-	void Draw_Laser_Inner_Part(HDC hdc, double x);
-	void Draw_Laser_Leg(HDC hdc, double x_pos, bool is_left);
-	void Draw_Laser_Cabin(HDC hdc, double x);
-
-	void Draw_Expanding_Figure(HDC hdc, EFigure_Type figure_type, double start_x, double start_y, double start_width, double start_height, double ratio, double end_x, double end_y, double end_width, double end_height);
-	int Get_Expanding_Value(double start, double end, double ratio);
-	double Get_Gun_Pos(double platform_x_pos, bool is_left);
-
-	bool Enable_Laser_Firing;
-	AsPlatform_State* Platform_State;
-	AColor* Inner_Color, * Circle_Color; // UNO, Use, Not Own!
-	AColor* Gun_Color;
-
-	AsLaser_Beam_Set *Laser_Beam_Set; // UNO
-	int Last_Laser_Shot_Time;
-	int Laser_Transformation_Step;
-
-	static const int Max_Laser_Transformation_Step = 20;
-	static const int Laser_Shot_Timeout = AsConfig::FPS / 2; // currently 20 FPS, so laser can shoot each 0.5 seconds
-};
 //------------------------------------------------------------------------------------------------------------
 class AsPlatform : public AHit_Checker, public AMover, public AGraphics_Object
 {
