809c445b9c490a9d89760ad60769f82685e31140
700grains <60423788+700grains@users.noreply.github.com>
2023-01-30 00:45:23 +0300

    Moving the AsPlatform_Expanding class to a separate pair of files

--- Popcorn/Platform.h
+++ Popcorn/Platform.h
@@ -4,31 +4,9 @@
 #include "Laser_Beam_Set.h"
 #include "Platform_State.h"
 #include "Platform_Glue.h"
+#include "Platform_Expanding.h"
 
-//------------------------------------------------------------------------------------------------------------
-class AsPlatform_Expanding
-{
-public:
-	~AsPlatform_Expanding();
-	AsPlatform_Expanding(AsPlatform_State& platform_state);
 
-	void Init(AColor& highlight_color, AColor& circle_color, AColor& inner_color);
-	bool Act(double &x_pos, EPlatform_State& next_state, bool& correct_pos);
-	void Draw_State(HDC hdc, double& x);
-	void Draw_Circle_Highlight(HDC hdc, int x, int y);
-	void Reset();
-
-	double Expanding_Platform_Width;
-private:
-	void Draw_Expanding_Platform_Ball(HDC hdc, double x, bool is_left);
-	void Draw_Expanding_Truss(HDC hdc, RECT& inner_rect, bool is_left);
-	
-	AsPlatform_State* Platform_State;
-	AColor* Inner_Color, * Highlight_Color, * Circle_Color; // UNO, Use, Not Own!
-	AColor *Truss_Color;
-
-	static const double Min_Expanding_Platform_Width, Max_Expanding_Platform_Width, Expanding_Platform_Width_Step;
-};
 //------------------------------------------------------------------------------------------------------------
 class AsPlatform_Laser
 {
