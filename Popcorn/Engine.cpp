0b28d4171bc513568dcf4c2620a3dc2f735bedf7
700grains <60423788+700grains@users.noreply.github.com>
2023-02-07 02:57:51 +0300

    Add a set of monsters to the Engine

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -49,6 +49,7 @@ void AsEngine::Init_Engine(HWND hwnd)
 	Add_Next_Module(index, &Platform);
 	Add_Next_Module(index, &Ball_Set);
 	Add_Next_Module(index, &Laser_Beam_Set);
+	Add_Next_Module(index, &Monster_Set);
 }
 //------------------------------------------------------------------------------------------------------------
 void AsEngine::Draw_Frame(HDC hdc, RECT &paint_area)
--- Popcorn/Engine.h
+++ Popcorn/Engine.h
@@ -6,6 +6,7 @@
 #include "Level.h"
 #include "Platform.h"
 #include "Ball_Set.h"
+#include "Monster_Set.h"
 
 //------------------------------------------------------------------------------------------------------------
 enum class EKey_Type: unsigned char
@@ -54,6 +55,7 @@ private:
 	AsBorder Border;
 	AsBall_Set Ball_Set; 
 	AsLaser_Beam_Set Laser_Beam_Set;
+	AsMonster_Set Monster_Set;
 
 	AGame_Object* Modules[AsConfig::Max_Modules_Count]; // UNO; main graphic modules
 };
