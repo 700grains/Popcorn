cca57fbb0f2706c115c7b7b2fff5c42affa2ead3
700grains <60423788+700grains@users.noreply.github.com>
2023-02-07 02:53:38 +0300

    Add the AMonster class

--- Popcorn/Monster_Set.h
+++ Popcorn/Monster_Set.h
@@ -3,6 +3,20 @@
 #include "Config.h"
 
 //------------------------------------------------------------------------------------------------------------
+class AMonster: public AGame_Object
+{
+public:
+	virtual void Begin_Movement();
+	virtual void Finish_Movement();
+	virtual void Advance(double max_speed);
+	virtual double Get_Speed();
+
+	virtual void Act();
+	virtual void Clear(HDC hdc, RECT& paint_area);
+	virtual void Draw(HDC hdc, RECT& paint_area);
+	virtual bool Is_Finished();
+};
+//------------------------------------------------------------------------------------------------------------
 class AsMonster_Set : public AsGame_Objects_Set
 {
 private:
