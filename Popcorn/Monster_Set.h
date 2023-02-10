260c454bf1bcd654feb647c6758e3735b02c00b1
700grains <60423788+700grains@users.noreply.github.com>
2023-02-10 03:21:12 +0300

    We move the AExplosive_Ball class into separate pair of files.

--- Popcorn/Monster_Set.h
+++ Popcorn/Monster_Set.h
@@ -19,47 +19,6 @@ enum class EMonster_State : unsigned char
 	Destroying
 };
 //------------------------------------------------------------------------------------------------------------
-enum class EExplosive_Ball_State : unsigned char
-{
-	Idle,
-	Charging,
-	Expanding,
-	Fading
-};
-//------------------------------------------------------------------------------------------------------------
-class AExplosive_Ball : public AGraphics_Object
-{
-public:
-	AExplosive_Ball();
-
-	virtual void Act();
-	virtual void Clear(HDC hdc, RECT& paint_area);
-	virtual void Draw(HDC hdc, RECT& paint_area);
-	virtual bool Is_Finished();
-
-	void Explode(int x_pos, int y_pos, int size, bool is_red, int time_offset, int step_count);
-
-	static void Setup_Colors();
-
-private:
-	void Update_Ball_Rect();
-
-	EExplosive_Ball_State Explosive_Ball_State;
-
-	bool Is_Red;
-	int X_Pos, Y_Pos;
-	int Step_Count;
-	int Start_Fading_Tick, Start_Explosion_Tick;
-	int Time_Offset;
-	double Max_Size, Size, Size_Step;
-	RECT Ball_Rect;
-
-	static const int Fading_Time = AsConfig::FPS;
-	static const int Max_Fade_Step = AsConfig::FPS;
-	static AColor Fading_Red_Colors[Max_Fade_Step];
-	static AColor Fading_Blue_Colors[Max_Fade_Step];
-};
-//------------------------------------------------------------------------------------------------------------
 class AMonster : public AGame_Object
 {
 public:
