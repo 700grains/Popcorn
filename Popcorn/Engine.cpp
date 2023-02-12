e6315bcf76fa2e0fdaaa4b8b84291cf4939c35b5
700grains <60423788+700grains@users.noreply.github.com>
2023-02-13 02:45:42 +0300

    Making the level stop correctly in Play_Level() when the ball is lost

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -146,6 +146,8 @@ void AsEngine::Play_Level()
 	{ // All balls are lost!
 		Game_State = EGame_State::Lost_Ball;
 		Level.Stop();
+		Monster_Set.Destroy_All();
+		Laser_Beam_Set.Disable_All();
 		Platform.Set_State(EPlatform_State::Meltdown);
 	}
 	else
