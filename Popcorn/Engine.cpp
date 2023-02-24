a994854cb2856be4980bd7a9a5dac71f3ee46410
Nerin <60423788+700grains@users.noreply.github.com>
2023-02-25 02:54:30 +0300

    Create 4th level

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -40,7 +40,7 @@ void AsEngine::Init_Engine(HWND hwnd)
 
 	AsPlatform::Hit_Checker_List.Add_Hit_Checker(&Monster_Set);
 
-	Level.Set_Current_Level(3);
+	Level.Set_Current_Level(4);
 
 	Platform.Redraw_Platform();
 
