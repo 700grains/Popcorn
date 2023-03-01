0dbc967c741b275d95ed4f88694d7e1012e6cfdc
Nerin <60423788+700grains@users.noreply.github.com>
2023-03-01 05:18:07 +0300

    Move the AMop_Indicator to separate pair of files

--- Popcorn/Mop.h
+++ Popcorn/Mop.h
@@ -29,32 +29,6 @@ private:
 	static const int Min_Height = Binding_Height + 1;
 };
 //------------------------------------------------------------------------------------------------------------
-class AMop_Indicators : public AGraphics_Object
-{
-public:
-	AMop_Indicators(int x_pos, int y_pos, int time_offset);
-
-	virtual void Act();
-	virtual void Clear(HDC hdc, RECT& paint_area);
-	virtual void Draw(HDC hdc, RECT& paint_area);
-	virtual bool Is_Finished();
-
-	void Set_Y_Pos(int y_pos);
-
-private:
-	int X_Pos, Y_Pos;
-	RECT Indicator_Rect;
-	const AColor* Current_Color;
-	int Time_Offset;
-
-	static const int Width = 17;
-	static const int Height = 5;
-	static const int Max_Fade_Step = AsConfig::FPS * 4 / 10; // 0.4 seconds 
-	static const int Normal_Timeout = AsConfig::FPS; // 1 sec
-
-	static AColor_Fade Fading_Blue_Colors;
-};
-//------------------------------------------------------------------------------------------------------------
 class AsMop : public AGame_Object
 {
 public:
